92f3aa16ac967470f4f87d5c371160439454134e
Siu Lun Chau <alanchau@hotmail.co.uk>
2023-06-02 13:01:16 +0200

    added visualisation function;

--- src/utils/visualisation/deterministic_values.py
+++ src/utils/visualisation/deterministic_values.py
@@ -1,8 +1,5 @@
-import matplotlib.pylab as plt
-import pandas as pd
 import shap
 import torch
-from torch import FloatTensor
 
 
 def summary_plot(shapley_values: torch.Tensor,
@@ -11,19 +8,3 @@ def summary_plot(shapley_values: torch.Tensor,
                  ):
     return shap.summary_plot(shapley_values.T.numpy(), query_data.numpy(), show=False,
                              **kwargs)
-
-
-def global_importance_bar_plot(mean_shapley_values: FloatTensor, feature_names: list[str], stds: FloatTensor,
-                               topk: int = 10):
-    global_explanations = pd.DataFrame(mean_shapley_values, index=feature_names, columns=["mean_svs"])
-    global_explanations["stds"] = stds
-    global_explanations.sort_values("mean_svs", ascending=True, inplace=True)
-
-    global_explanations = global_explanations.iloc[-topk:, :]
-
-    plt.barh(global_explanations.index, global_explanations["mean_svs"], alpha=0.5)
-    plt.errorbar(global_explanations["mean_svs"], global_explanations.index, xerr=global_explanations.stds, ls="none")
-    plt.title("Global contribution with 1 sd error bar")
-    plt.ylabel("Global Feature contribution")
-
-    return None
--- src/utils/visualisation/stochastic_values.py
+++ src/utils/visualisation/stochastic_values.py
@@ -1,4 +1,5 @@
 import matplotlib.pylab as plt
+import pandas as pd
 import torch
 
 from src.explanation_algorithms.BayesGPSHAP import BayesGPSHAP
@@ -9,6 +10,14 @@ def local_explanation_plot(data_id: int,
                            explanation_algorithm: BayesGPSHAP,
                            uncertainty_source: str,
                            coverage: float = 1.0):
+    """plot local explanation given specific data id
+
+    :param data_id: the index of the observation you wish to explain
+    :param feature_names: list of feature names
+    :param explanation_algorithm: the BayesGPSHAP object
+    :param uncertainty_source: either "GPSHAP", "BayesSHAP", or "BayesGPSHAP".
+    :param coverage: the scalar multiplying the standard deviation. Default to 1 will give you a coverage to 68% of the data.
+    """
     X = explanation_algorithm.X_explained
     mean_stochastic_shapley_values = explanation_algorithm.return_mean_stochastic_shapley_values()
 
@@ -21,9 +30,49 @@ def local_explanation_plot(data_id: int,
 
     uncertainties = coverage * torch.diag(uncertainties).sqrt()
 
-    plt.bar(feature_names, mean_stochastic_shapley_values[:, data_id], alpha=0.5)
-    plt.errorbar(feature_names, mean_stochastic_shapley_values[:, data_id], yerr=uncertainties, capsize=3, ls="None")
+    local_importance = pd.DataFrame(mean_stochastic_shapley_values[:, data_id], index=feature_names, columns=["mean"])
+    local_importance["std"] = uncertainties
+    local_importance = local_importance.sort_values("mean")
+
+    plt.barh(local_importance.index, local_importance["mean"], alpha=0.5)
+    plt.errorbar(y=local_importance.index, x=local_importance["mean"], xerr=local_importance["std"], capsize=3,
+                 ls="None")
     plt.title(f"Stochastic Explanations for data {data_id} ({uncertainty_source})")
-    plt.xlabel("feature names")
-    plt.ylabel("stochastic Shapley values")
+    plt.xlabel("stochastic Shapley values")
+    plt.tight_layout()
+    plt.show()
+
+
+def global_explanation_plot(feature_names: list[str],
+                            explanation_algorithm: BayesGPSHAP,
+                            uncertainty_source: str,
+                            coverage: float = 1.0):
+    """plot global explanation based on the distribution of the absolute Stochastic Shapley values
+
+        :param data_id: the index of the observation you wish to explain
+        :param feature_names: list of feature names
+        :param explanation_algorithm: the BayesGPSHAP object
+        :param uncertainty_source: either "GPSHAP", "BayesSHAP", or "BayesGPSHAP".
+        :param coverage: the scalar multiplying the standard deviation. Default to 1 will give you a coverage to 68% of the data.
+        """
+    means, stds, cov_mat = explanation_algorithm.compute_global_feature_importances_with_different_uncertainties(
+        sample_size=10000, uncertainty_source=uncertainty_source
+    )
+
+    global_importance = pd.DataFrame(means, index=feature_names, columns=["mean"])
+    global_importance["std"] = stds
+    global_importance = global_importance.sort_values("mean")
+
+    plt.barh(global_importance.index, global_importance["mean"], alpha=.5, color="blue")
+    plt.errorbar(x=global_importance["mean"],
+                 y=global_importance.index,
+                 xerr=coverage * global_importance["std"],
+                 ls="none",
+                 color="blue",
+                 capsize=3
+                 )
+    plt.xlabel("Mean Absolute Stochastic Shapley Values")
+    plt.title("Global Explanations")
+    plt.ylabel("")
+    plt.tight_layout()
     plt.show()
